be12c9675ba2206346759f125599010c0ed1d312
ilammy <a.lozovsky@gmail.com>
2018-03-13 00:44:34 +0200

    Camera preview
    Add a CameraPreview view which streams the current camera vision.
    Create the preview on activity start and stop the capture when we
    are paused. Releasing the camera is actually very important.

---
+++ unhaze/src/main/java/net/ilammy/unhaze/CameraPreview.java
@@ -0,0 +1,48 @@
+package net.ilammy.unhaze;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+
+    private Camera m_camera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+
+        m_camera = camera;
+
+        SurfaceHolder holder = getHolder();
+        holder.addCallback(this);
+
+        // Required for Android pre-3.0:
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        try {
+            m_camera.setPreviewDisplay(surfaceHolder);
+            m_camera.startPreview();
+        } catch (IOException e) {
+            Log.e(TAG, "failed to set camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        // Currently we ignore any change events.
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        // We'll destroy the camera in the activity.
+    }
+}
