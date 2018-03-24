f0919fea59c2ad8a976315704beac3a19b0c3002
ilammy <a.lozovsky@gmail.com>
2018-03-24 17:22:38 +0200

    Improve rotation stability
    Increase sampling rate as this seems to improve the output of the
    sensor. Also add some simple smoothing of the sensor data so that
    the changes are not propagated instantly. This gives some minor
    improvements in rotation stability.
    
    Maybe including positional data will also have some effect on the
    visible image. Accelerometer integration is next.

--- unhaze/src/main/java/net/ilammy/unhaze/SpatialAwareness.java
+++ unhaze/src/main/java/net/ilammy/unhaze/SpatialAwareness.java
@@ -10,14 +10,15 @@ import android.opengl.GLSurfaceView;
 
 public class SpatialAwareness implements SensorEventListener {
 
-    private static final int SAMPLES_PER_SECOND = 60;
+    private static final int SAMPLES_PER_SECOND = 120;
     private static final int SAMPLING_PERIOD_US = 1_000_000 / SAMPLES_PER_SECOND;
-
+    // (0.0, 1.0) range where 0.0 is no updates at all, 1.0 is no smoothing at all.
+    private static final float SMOOTH_FACTOR = 0.75f;
     private SensorManager m_sensorManager;
     private Sensor m_rotationSensor;
-
     private GLSurfaceView m_surfaceView;
     private OverlayRenderer m_renderer;
+    private float[] m_rotationVector;
 
     public SpatialAwareness(Activity activity) {
         m_sensorManager = (SensorManager) activity.getSystemService(Context.SENSOR_SERVICE);
@@ -40,9 +41,22 @@ public class SpatialAwareness implements SensorEventListener {
         m_sensorManager.unregisterListener(this);
     }
 
+    private void updateRotationVector(float[] newValues) {
+        // Smooth the readings as the rotation sensor is kinda noisy.
+        if (m_rotationVector == null) {
+            m_rotationVector = newValues;
+        } else {
+            for (int i = 0; i < m_rotationVector.length; i++) {
+                m_rotationVector[i] += SMOOTH_FACTOR * (newValues[i] - m_rotationVector[i]);
+            }
+        }
+    }
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        m_renderer.setRotationVector(sensorEvent.values);
+        updateRotationVector(sensorEvent.values);
+
+        m_renderer.setRotationVector(m_rotationVector);
         m_surfaceView.requestRender();
     }
 
